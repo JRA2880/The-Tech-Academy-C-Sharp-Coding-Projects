b226979862e5e28fbd93d430e64c439d7452f144
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-09-10 23:16:10 -0400

    Upload interview challenge reversed string app to GitHub

---
+++ ReversedStringApp/ReversedStringApp/Program.cs
@@ -0,0 +1,40 @@
+﻿//Programer: John R. Adams
+//Date: 09-10-2019
+//
+//Task: Given a string, reverse it.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedStringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forwardString;
+            string reversedString;
+
+            forwardString = "nacirema";
+            reversedString = "";
+
+            Console.WriteLine("String is {0}", forwardString);
+
+            // find string length
+            int len;
+            len = forwardString.Length - 1;
+
+            while (len >= 0)
+            {
+                reversedString = reversedString + forwardString[len];
+                len--;
+            }
+            Console.WriteLine("Reversed String is {0}", reversedString);
+            Console.ReadLine();
+        }
+    }
+}
