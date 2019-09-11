91da62f4d50436665cd172a41849280bf090978a
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-09-10 23:03:13 -0400

    Upload new challenge to GitHub

---
+++ numArraySum/numArraySum/Program.cs
@@ -0,0 +1,31 @@
+﻿//Programmer: John R. Adams
+//Date: 9/10/2019
+//
+//Interview Task: Given an array of integers, write a method to sum the elements in the array, knowing that some of the elements may be very large integers.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int arraySum = 0;
+
+            int[] numArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,1000,2500,3000,450000};
+
+            for (i = 0; i < numArray.Length; i++)
+            {
+                arraySum = arraySum + numArray[i];
+            }
+            Console.WriteLine("The sum of the numbers in the number array is: {0}", arraySum);
+        }
+    }
+}
