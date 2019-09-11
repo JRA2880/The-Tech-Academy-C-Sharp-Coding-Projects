128b3d64364adbf0e0e763fadefc5b0a55db162e
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-09-10 22:50:10 -0400

    Upload CSharp Interview Challenge oddSum Array to GitHub

---
+++ oddArraySum/oddArraySum/Program.cs
@@ -0,0 +1,34 @@
+﻿//Programmer: John R. Adams
+//Date: 9/10/2019
+//
+//Interview Task: Given an array of integers, write a method to total the odd numbers.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int oddSum = 0;
+            
+            int[] numArray = new int[] { 1, 2, 3,5,6,7,8,9};
+
+            for(i = 0; i < numArray.Length; i++)
+            {
+                if (numArray[i] % 2 > 0)
+                {
+                    oddSum = oddSum + numArray[i];
+                }
+            }
+            Console.WriteLine("The sum of the odd numbers in the number array is: {0}", oddSum);     
+        }
+    }
+}
