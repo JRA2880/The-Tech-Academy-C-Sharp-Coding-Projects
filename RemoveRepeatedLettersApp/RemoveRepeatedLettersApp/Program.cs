33c188a5005eec83fd14fe5de4cb6b52dbf4dd5d
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-09-10 23:33:01 -0400

    Upload repeated letter challenge to GitHub

---
+++ RemoveRepeatedLettersApp/RemoveRepeatedLettersApp/Program.cs
@@ -0,0 +1,49 @@
+﻿//Programmer: John R. Adams
+//Date: 9-10-2019
+//
+//Task: Given a string, remove any repeated letters.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveRepeatedLettersApp
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string word1 = RemoveRepeatedLetters("Jabberwacky");
+            string word2 = RemoveRepeatedLetters("Ninja");
+            string word3 = RemoveRepeatedLetters("Racoon");
+            string word4 = RemoveRepeatedLetters("There once was a man named Othello, who was a fine fellow...");
+            string word5 = RemoveRepeatedLetters("You can learn to code anywhere in the world!");
+
+            Console.WriteLine(word1);
+            Console.WriteLine(word2);
+            Console.WriteLine(word3);
+            Console.WriteLine(word4);
+            Console.WriteLine(word5);
+        }
+        static string RemoveRepeatedLetters(string word)
+        {
+            string newWord = "";
+
+            foreach (char character in word)
+            {
+                if (newWord.IndexOf(character) == -1)
+                {
+                    newWord += character;
+                }
+            }
+            return newWord;
+        }
+
+    }
+
+}
+
+
