d85ced7b8cff6dc74b93038324ae3a58444e6e80
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-09-10 23:37:51 -0400

    Upload FizzBuzz Challenge to GitHub

---
+++ FizzBuzzCSharp/FizzBuzzCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿//Programmer: John R. Adams
+//Date: 09-10-2019
+//Task: FizzBuzz in C#
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
